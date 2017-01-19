d58dca2de8cb2e13c7057e484e8159305d9cda1d
Keenan Jaenicke <keenan.jaenicke@moserit.com>
2017-01-19 13:59:30 -0500

    fixes bug defaulting to sessionStorage removal of token instead of option

--- src/signout.js
+++ src/signout.js
@@ -5,7 +5,7 @@ import { clearToken } from './storage'
 * Clears token from localStorage, informs store of signout and redirects to login
 */
 export default function* signout(error, options) {
-  yield call(clearToken);
+  yield call(clearToken, options.storageType);
   yield put(options.onLogoutAction(error));
 
   if(options.redirectToOnLogout){
